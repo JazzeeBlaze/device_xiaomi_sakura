e86e4316b9e804ad4250c8ec0336ceaced7d5b61
Jeevesh V <91652064+JazzeeBlaze@users.noreply.github.com>
2023-08-16 11:36:59 +0530

    Update nad_sakura.mk

--- nad_sakura.mk
+++ nad_sakura.mk
@@ -1,41 +1,24 @@
-#
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-#
+
 # All components inherited here go to system_ext image
-#
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-# Inherit from mido device
+# Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
 # Inherit some common nadOS stuff.
 $(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
-# Poduct spec
+# Product spec
 IS_PHONE := true
 scr_resolution := 1080
 
 # Device identifier. This must come after all inclusions
-DEVICE_MAINTAINER := Jeevesh
 PRODUCT_DEVICE := sakura
 PRODUCT_NAME := nad_sakura
 BOARD_VENDOR := Xiaomi
@@ -45,6 +28,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_INCLUDE_STOCK_ARCORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 # Face Unlock Flag
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -55,5 +40,9 @@ TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS=true
-#Official
-ro.nad.maintainer=JEEVESH
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sakura-user 9 PKQ1.180917.001 V12.0.1.0.PDICNXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
